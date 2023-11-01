967c2aaad6b13708a693adb6ffd98cef69542140
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-01 17:59:57 +0100

    update recent posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'user_id'
 
-  def user_recent_posts()
+  def recent_posts()
     Post.order('created_at Desc').limit(3)
   end
 end
