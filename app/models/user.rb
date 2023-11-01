31b2e0c8cb75bae9163d241d2c8be1e7df4a1786
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-01 17:57:47 +0100

    add required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'user_id'
 
-  def user_recent_posts(user)
+  def user_recent_posts()
     Posts.order('created_at Desc').limit(3)
   end
 end
