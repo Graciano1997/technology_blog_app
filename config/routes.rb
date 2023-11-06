cbebd59f094129439b560a45612a4a7be0974067
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-06 17:31:33 +0100

    update required changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "users#index"
-  # get '/user/:id', to: "users#index"
   resources :users, only [:index, :show] do
     resources :posts, only: [:index, :show]
   end
