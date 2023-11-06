7d55bcf40151635cf200295b9cbb7743ba953537
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-06 16:55:10 +0100

    add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @id=params[:user_id]
   end
+
+  def show
+    @user_id=params[:user_id]
+    @id=params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
   # get '/user/:id', to: "users#index"
-  resources :users, shallow: true do
+  resources :users do
     resources :posts
   end
   get "up" => "rails/health#show", as: :rails_health_check
