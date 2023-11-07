0269fbe1354278482dae45d0fb951ce8cc6f82b7
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-07 13:47:37 +0100

    fix linter issues

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,11 @@
 describe "GET user 's posts /" do
-  it "should return a successfull response" do
-  get '/users/2/posts'
-  expect(response).to be_successful
+  it 'should return a successfull response' do
+    get '/users/2/posts'
+    expect(response).to be_successful
   end
 
-  it "should return a successfull response" do
-  get '/users/2/posts/1000'
-  expect(response).to be_successful
+  it 'should return a successfull response' do
+    get '/users/2/posts/1000'
+    expect(response).to be_successful
   end
 end
