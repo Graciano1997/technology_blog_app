97abb264348abe240f990aaa441c06273b7fb3ea
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-09 15:04:10 +0100

    fix linter issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @user=User.find(params[:user_id])
-    @user_posts=Post.where(author_id: params[:user_id]).all
+    @user = User.find(params[:user_id])
+    @user_posts = Post.where(author_id: params[:user_id]).all
   end
 
   def show
-    @id=params[:id]
-    @user=User.find(params[:user_id])
-    @post_item=Post.where(author_id: @user.id)[params[:id].to_i - 1] if params[:id].to_i
+    @id = params[:id]
+    @user = User.find(params[:user_id])
+    @post_item = Post.where(author_id: @user.id)[params[:id].to_i - 1] if params[:id].to_i
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  def index;
-  @users=User.all
+  def index
+    @users = User.all
   end
 
   def show
-    @user=User.find(params[:id].to_i)
+    @user = User.find(params[:id].to_i)
   end
 end
