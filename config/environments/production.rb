a7c9b5b62d250845bdfc27d90ad8956b2879d1cd
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-16 20:28:05 +0100

    style the forms

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
@@ -94,4 +95,16 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'gracianomanuelhenrique@gmail.com',
+  password: 'ccnt wnob qccp dxzw',
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
+
 end
