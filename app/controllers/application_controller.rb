67334f75796fa78cf14f97669172831d65e145fb
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-17 00:49:31 +0100

    add email configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :name, :password,:posts_counter)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :name, :password, :current_password)}
-  end
+   def update_allowed_parameters
+     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :name, :password,:posts_counter)}
+     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :name, :password, :current_password)}
+   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,15 +96,16 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'gmail.com',
-  user_name: 'gracianomanuelhenrique@gmail.com',
-  password: 'ccnt wnob qccp dxzw',
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
+ config.action_controller.raise_on_missing_callback_actions = true
+     config.action_mailer.delivery_method = :smtp
+     config.action_mailer.smtp_settings = {
+     address: 'smtp.gmail.com',
+     port: 587,
+     domain: 'gmail.com',
+     user_name: 'gracianomanuelhenrique@gmail.com',
+     password: 'ucaz lxzk yrxe ises',
+     authentication: 'plain',
+     enable_starttls_auto: true
+   }
 
 end
