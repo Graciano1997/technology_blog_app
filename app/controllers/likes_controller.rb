1941ccbe4b32a4516f27df497af112bfcabd76fd
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-17 09:38:26 +0100

    fix like issues

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,11 @@
 class LikesController < ApplicationController
   def create
     @user = User.where(email: session[:user_email]).first
+    @like = Like.create(user: @user, post_id: params[:post_item_id])
+    if @like.new_record?
+      puts 'Ups!can not like'
+    else
+      puts 'One more like'
+    end
   end
 end
