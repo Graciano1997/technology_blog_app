8fc471905af9e4e8735210b2f412fe308217f447
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-17 15:41:31 +0100

    update user session key

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def new
-    @user = User.where(email: session[:user_email]).first
+    @user = User.where(email: session[:user]["email"]).first
     @comment = @user.posts[params[:post_id].to_i - 1].comments.new
   end
-
+  
   def create
-    @comment = Comment.create(text: params[:comment][:text], user: current_user, post_id: params[:post_id])
+    @user = User.where(email: session[:user]["email"]).first
+    @comment = Comment.create(text: params[:comment][:text], user: @user, post_id: params[:post_id])
     if @comment.new_record?
       redirect_to "/users/#{params[:user_id]}/posts/#{params[:comment][:url_id]}",
                   flash: { wrong: 'Upps! Comment was not created.' }
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,29 @@
 class PostsController < ApplicationController
   def index
-    @user = User.where(email: session[:user_email]).first
+    @user = User.where(email: session[:user]["email"]).first
     @user_posts = Post.includes(:author).where(author_id: @user.id)
   end
 
   def show
     @id = params[:id]
-    @user = User.where(email: session[:user_email]).first
+    @user = User.where(email: session[:user]["email"]).first
     @post_item = Post.includes(:author).where(author: @user, posts: { id: params[:id] }).first
     @comment = @user.posts[params[:post_id].to_i - 1].comments.new
   end
 
   def new
-    @user = User.where(email: session[:user_email]).first
+    @user = User.where(email: session[:user]["email"]).first
     @post = @user.posts.new
   end
 
   def create
-    @user = User.where(email: session[:user_email]).first
+    @user = User.where(email: session[:user]["email"]).first
     @post = Post.create(author: @user, title: params[:post][:title], text: params[:post][:text],
                         comments_counter: 0, likes_counter: 0)
     if @post.new_record?
-      redirect_to '/users/1/posts/new', flash: { wrong: 'Upps! Post was not created.' }
+      redirect_to "/users/#{@user.id}/posts/new", flash: { wrong: 'Upps! Post was not created.' }
     else
-      redirect_to '/users/1/posts/new', flash: { success: 'Post was successfully created.' }
+      redirect_to "/users/#{@user.id}/posts/new", flash: { success: 'Post was successfully created.' }
     end
   end
 end
