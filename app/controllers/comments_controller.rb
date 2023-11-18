409e5154731c307c54e65a19e130e00d156082d2
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-18 01:07:18 +0100

    add delete comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     @user = User.where(email: session[:user]["email"]).first
     @comment = @user.posts[params[:post_id].to_i - 1].comments.new
   end
-  
+
   def create
     @user = User.where(email: session[:user]["email"]).first
     @comment = Comment.create(text: params[:comment][:text], user: @user, post_id: params[:post_id])
@@ -14,5 +14,14 @@ class CommentsController < ApplicationController
       redirect_to "/users/#{params[:user_id]}/posts/#{params[:comment][:url_id]}",
                   flash: { success: 'Comment was successfully created.' }
     end
+  end
+
+  def destroy
+    puts "Destruindo o comentario..."
+    @comment=Comment.find(params[:comment_item].to_i)
+    @post=Post.find(@comment.post_id)
+    @comment.destroy
+    @post.decrement!(:comments_counter)
+    redirect_to "/users/#{session[:user]["id"]}/posts/@post.id", notice: 'Comment was successfully destroyed.'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root "users#index"
   get 'logout', to: "users#logout"
   delete '/delete/post/:id', to: "posts#destroy"
+  delete '/delete/comment/:id', to: "comments#destroy"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:new, :create, :destroy]
     resources :likes, only: [:create]
     end
   end
