1bfaaf1deae7767cf816103ca3bcc5515750ddb8
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-18 16:20:27 +0100

    fix linter issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,14 +20,14 @@ class PostsController < ApplicationController
   def create
     authorize! :create, @post
     puts params.inspect
-     @user = User.where(email: session[:user]['email']).first
-     @post = Post.create(author: @user, title: params[:post][:title], text: params[:post][:text],
-                         comments_counter: 0, likes_counter: 0)
-     if @post.new_record?
-       redirect_to "/users/#{@user.id}/posts/new", flash: { wrong: 'Upps! Post was not created.' }
-     else
-       redirect_to "/users/#{@user.id}/posts/new", flash: { success: 'Post was successfully created.' }
-     end
+    @user = User.where(email: session[:user]['email']).first
+    @post = Post.create(author: @user, title: params[:post][:title], text: params[:post][:text],
+                        comments_counter: 0, likes_counter: 0)
+    if @post.new_record?
+      redirect_to "/users/#{@user.id}/posts/new", flash: { wrong: 'Upps! Post was not created.' }
+    else
+      redirect_to "/users/#{@user.id}/posts/new", flash: { success: 'Post was successfully created.' }
+    end
   end
 
   def destroy
@@ -44,6 +44,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:author, :title,:text)
+    params.require(:post).permit(:author, :title, :text)
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     can :destroy, Post, author: user
-    can :destroy, Comment, user: user
+    can(:destroy, Comment, user:)
     can :read, Post
     can :read, Comment
 
