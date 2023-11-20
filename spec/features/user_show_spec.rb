bc51618d4f266182c58ee59aa1f8db68bef0e509
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-11-20 16:45:55 +0545

    Some changes in user_show_spec.rb file

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,1 +1,7 @@
-# here
+require 'rails_helper'
+
+RSpec.describe 'Index', type: :feature do
+  before :each do
+    @user_gra = User.create(name: 'GracianoHenrique', photo: 'https://graciano.jpg', bio: 'Web Developer',
+                            posts_counter: 0)
+    @user_sha = User.create(name: 'Sha', photo: 'https://sha.jpg', bio: 'Web Developer', posts_counter: 0)
