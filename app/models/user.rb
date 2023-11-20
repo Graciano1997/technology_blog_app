578d14d62de60ef6282fd688207117aa107b6ee0
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-11-20 16:55:13 +0545

    Added required code to models.user.rb according to the requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,19 @@
-# user devise
+class User < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable, :confirmable
+    has_many :comments, foreign_key: 'user_id'
+    has_many :posts, foreign_key: 'author_id'
+    has_many :likes, foreign_key: 'user_id'
+    validates :name, presence: true
+    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    def user_recent_posts
+      posts.order('created_at Desc').limit(3)
+    end
+  
+    def find_user(id)
+      User.find(id)
+    end
+  end
+  
