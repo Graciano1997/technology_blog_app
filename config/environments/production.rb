68f7596b27e9b0a8b69d727d96e6d145252a8054
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-20 11:14:39 +0100

    update prodution configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,15 +87,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
  config.action_controller.raise_on_missing_callback_actions = true
      config.action_mailer.delivery_method = :smtp
      config.action_mailer.smtp_settings = {
