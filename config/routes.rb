d67822e94f02d5d2a3cb7810d76ee4947ff85750
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-21 03:10:29 +0100

    fix tests issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root "users#index"
-  get 'logout', to: "users#logout"
-  delete '/delete/post/:id', to: "posts#destroy"
-  delete '/delete/comment/:id', to: "comments#destroy"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:index, :new, :create, :destroy]
+    resources :posts, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:index, :new, :create]
     resources :likes, only: [:create]
     end
   end
   get "up", to: "rails/health#show", as: :rails_health_check
-  
-  namespace :api, default: {format: :json} do
-    namespace :v1 do
-      resources :posts, only: %i[index] do
-        resources :comments, only: %i[index create]
-      end
-    end
-  end
 end
