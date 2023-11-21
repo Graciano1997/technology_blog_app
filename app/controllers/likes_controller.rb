6e429a78d4fe2f99bfc94f594f59346f0f2c7f4b
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-21 01:27:02 +0100

    add requird challenge

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,8 @@
 class LikesController < ApplicationController
   def create
-    @user = User.where(email: session[:user]['email']).first
-    @like = Like.create(user: @user, post_id: params[:post_item_id])
+    @like = Like.create(user: current_user, post_id: params[:post_item_id])
     if @like.new_record?
-      puts 'Ups!can not like'
+      puts 'can not like'
     else
       puts 'One more like'
     end
