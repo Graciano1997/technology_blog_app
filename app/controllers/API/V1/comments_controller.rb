52164f8b4d69b5eed0686f8c1d456d9a6724cd3b
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-11-22 12:48:31 +0545

    added required code to the comment controller file

---
+++ app/controllers/API/V1/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+  
+    def index
+      @post = Post.includes(:comments).find(params[:post_id])
+      @comments = @post.comments
+      render json: { success: true, data: { comments: @comments } }
+    end
+  
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.new(text: params[:text], user_id: current_user.id)
+  
+      if @comment.save
+        render json: { success: true, data: { comment: @comment, user: current_user } }, status: :created
+      else
+        render json: { success: false, errors: @comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def params
+      params.require(:comment).permit(:text)
+    end
+  end
+  
