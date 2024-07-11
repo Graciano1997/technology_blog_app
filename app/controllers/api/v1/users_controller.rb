c29fbc6eca0db7c115945092653a2d182cf88fa2
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-07-11 11:24:13 -0400

    fix the linter

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostsController < ApplicationController
   load_and_authorize_resource
-  
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order('created_at')
