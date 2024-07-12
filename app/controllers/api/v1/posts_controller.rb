b29e545ea7df70238954aca833e009af645bed47
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-07-12 14:38:33 -0400

    fix the issue

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,15 @@
-class Api::V1::PostsController < ApplicationController
-  load_and_authorize_resource
-  skip_before_action :authenticate_user!
-  before_action :authenticate_request
-  after_action :hard_logout
+module Api
+  module V1
+    class Api::V1::PostsController < ApplicationController
+      load_and_authorize_resource
+      skip_before_action :authenticate_user!
+      before_action :authenticate_request
+      after_action :hard_logout
 
-  def index
-    @posts = User.find(params[:user_id]).posts.order('created_at')
-    render json: { success: true, data: { posts: @posts } }, status: :ok
+      def index
+        @posts = User.find(params[:user_id]).posts.order('created_at')
+        render json: { success: true, data: { posts: @posts } }, status: :ok
+      end
+    end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,13 @@
-class Api::V1::PostsController < ApplicationController
-  load_and_authorize_resource
+module Api
+  module V1
+    class PostsController < ApplicationController
+      load_and_authorize_resource
 
-  def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts.order('created_at')
-    render json: { success: true, data: { posts: @posts } }, status: :ok
+      def index
+        @user = User.find(params[:user_id])
+        @posts = @user.posts.order('created_at')
+        render json: { success: true, data: { posts: @posts } }, status: :ok
+      end
+    end
   end
 end
