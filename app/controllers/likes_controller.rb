2f4ebe8efb3ee2fc598ed97f368f85c05e538c14
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-07-14 18:07:24 -0400

    update the like logic

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,9 @@
 class LikesController < ApplicationController
   def create
     @user = User.where(email: session[:user]['email']).first
-
-    @zero = @user.posts[(params[:post_item_id].to_i - 1)].likes.where(user_id: @user.id).count.zero?
-    @like = Like.create(user: @user, post_id: params[:post_item_id]) if @zero
+    @post_item = Post.find(params[:post_id].to_i)
+    @posts_like = @post_item.likes
+    @zero = @post_item.likes.where(user_id: @user.id).count.zero? unless @posts_like.nil?
+    @like = Like.create(user: @user, post: @post_item) if @zero
   end
 end
